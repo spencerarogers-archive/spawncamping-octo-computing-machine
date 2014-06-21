fb187906b4a4cc49650a9002cb81df4701c9bb06
Spencer Rogers <spencer1248@gmail.com>
2014-06-21 13:47:55 -0400

    More loggin

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,8 +1,9 @@
 class GithubController < ApplicationController
   def pull_request
+    Rails.logger.info "[[#{params[:action].upcase} PULL REQUEST EVENT RECEIVED]]"
     if ['opened', 'reopened'].include?(params[:action])
       email = params[:pull_request][:title].strip
-      Rails.logger.info "[[PULL REQUEST RECEIVED FROM #{email}]]"
+      Rails.logger.info "[[PULL REQUEST FROM #{email}]]"
       PullRequestMailer.pr_received(email).deliver if email.match(/.+@.+/)
     end
 
