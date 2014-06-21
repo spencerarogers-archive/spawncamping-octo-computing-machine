c7a7f182a1b8b05cb3349190c6bc267bbfa4daa4
Spencer Rogers <spencer1248@gmail.com>
2014-06-21 13:51:41 -0400

    Remove check for action

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,11 +1,8 @@
 class GithubController < ApplicationController
   def pull_request
-    Rails.logger.info "[[#{params[:action].upcase} PULL REQUEST EVENT RECEIVED]]"
-    if ['opened', 'reopened'].include?(params[:action])
-      email = params[:pull_request][:title].strip
-      Rails.logger.info "[[PULL REQUEST FROM #{email}]]"
-      PullRequestMailer.pr_received(email).deliver if email.match(/.+@.+/)
-    end
+    email = params[:pull_request][:title].strip
+    Rails.logger.info "[[PULL REQUEST FROM #{email}]]"
+    PullRequestMailer.pr_received(email).deliver if email.match(/.+@.+/)
 
     render nothing: true
   end
