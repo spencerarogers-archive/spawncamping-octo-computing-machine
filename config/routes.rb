4db6a2cd5689896c23f101e6fd13ee4465268290
Spencer Rogers <spencer1248@gmail.com>
2014-06-21 13:03:43 -0400

    Update github pull request endpoing HTTP verb.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get 'secrets', to: 'application#secrets'
   get 'access_denied', to: 'application#access_denied', as: :access_denied
-  get 'github/pull_request', to: 'github#pull_request', as: :pull_request
+  post 'github/pull_request', to: 'github#pull_request', as: :pull_request
 end
