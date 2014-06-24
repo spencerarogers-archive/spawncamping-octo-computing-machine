598090ade8de0ee47830b3cb1d5a01a98f7ea281
zjkaufman <zjkaufman@wustl.edu>
2014-06-24 10:21:56 -0400

    Update 20140624043833_create_completions.rb

--- db/migrate/20140624043833_create_completions.rb
+++ db/migrate/20140624043833_create_completions.rb
@@ -2,6 +2,8 @@ class CreateCompletions < ActiveRecord::Migration
   def change
     create_table :completions do |t|
       t.string :display_name
+      
+      t.string: Zach Kaufman & Karyn Vilbig
 
       t.timestamps
     end
