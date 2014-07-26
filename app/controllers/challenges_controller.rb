43545e4c37d2e100cb35193b8cdfdaef7fe49bf1
Spencer Rogers <spencer1248@gmail.com>
2014-07-26 13:48:04 -0400

    Stub out challenge page with awesome title

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,4 @@
+class ChallengesController < ApplicationController
+  def dom
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'backdoor#secrets'
 
+  get  'dom-ination', to: 'challenges#dom'
   get  'secrets', to: 'backdoor#secrets'
   get  'access_denied', to: 'application#access_denied', as: :access_denied
   get  'encryption', to: 'encryption#new', as: :new_encryption
